480093da30a34b0a57623d427cbff78474bc6ed7
Bryan Hickey <bjh@bjh.dev>
2023-04-06 07:25:54 +1000

    remove unused vars

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -1,15 +1,15 @@
-import { Footer } from '@/components/global/Footer'
-import { Navbar } from '@/components/global/Navbar'
-import { PreviewBanner } from '@/components/preview/PreviewBanner'
-import { getSettings } from '@/lib/sanity.client'
-import { getPreviewToken } from '@/lib/sanity.server.preview'
+// import { Footer } from '@/components/global/Footer'
+// import { Navbar } from '@/components/global/Navbar'
+// import { PreviewBanner } from '@/components/preview/PreviewBanner'
+// import { getSettings } from '@/lib/sanity.client'
+// import { getPreviewToken } from '@/lib/sanity.server.preview'
 
 export default async function IndexRoute({
 	children,
 }: {
 	children: React.ReactNode
 }) {
-	const token = getPreviewToken()
+	// const token = getPreviewToken()
 	// const settings = (await getSettings({ token })) || {
 	// 	menuItems: [],
 	// }
