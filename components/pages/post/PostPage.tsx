f759e6a46dc62206da15c632d1ab55c14fa2eeed
Bryan Hickey <bjh@bjh.dev>
2023-05-01 18:50:25 +1000

    fix linting errors

--- components/pages/post/PostPage.tsx
+++ components/pages/post/PostPage.tsx
@@ -1,16 +1,15 @@
-import { Header } from '@/components/shared/Header'
 import ScrollUp from '@/components/shared/ScrollUp'
 import type { PostPayload } from '@/lib/types'
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function PostPage({ data }: { data: PostPayload }) {
 	// Default to an empty object to allow previews on non-existent documents
-	const { overview, title } = data || {}
+	// const { overview, title } = data || {}
 
 	return (
 		<div>
 			<div className='mb-20 space-y-6'>
 				{/* Header */}
-				<Header title={title} description={overview} />
 				{/* Workaround: scroll to top on route change */}
 				<ScrollUp />
 			</div>
--- sanity/schemas/documents/page.ts
+++ sanity/schemas/documents/page.ts
@@ -1,5 +1,5 @@
 import { DocumentIcon } from '@sanity/icons'
-import { defineArrayMember, defineField, defineType } from 'sanity'
+import { defineField, defineType } from 'sanity'
 
 export default defineType({
 	type: 'document',
